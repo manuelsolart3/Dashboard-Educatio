49ceabf08f21c1d7906ea47e3e4073f98c61e69f
manuelsolart3 <118754640+manuelsolart3@users.noreply.github.com>
2024-04-12 14:43:55 -0500

    ruta protegida

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //Layout
 import LayoutAdmin from "./layout/LayoutAdmin";
@@ -14,52 +14,64 @@ import Error404 from "./pages/Error404";
 import Verificacion from "./pages/admin/Verificacion";
 
 
+
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  // Lógica para verificar si el usuario está autenticado
-  const checkAuthentication = () => {
-    const token = localStorage.getItem("accessToken");
-    setIsAuthenticated(!!token); // Establece isAuthenticated en verdadero si hay un token
-  };
-
-  // Llama a checkAuthentication cuando se monta la aplicación para verificar la autenticación
-  useState(() => {
+  useEffect(() => {
     checkAuthentication();
   }, []);
 
+  const checkAuthentication = () => {
+    const token = localStorage.getItem("accessToken");
+    setIsAuthenticated(!!token);
+  };
+
+  const handleLogin = async (email, password) => {
+    try {
+      const response = await fetch("https://bdeducatio.vercel.app/api/admin/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
+      });
+
+      if (!response.ok) {
+        throw new Error("Usuario o contraseña incorrectos");
+      }
+
+      const data = await response.json();
+      const accessToken = data.accessToken;
+      localStorage.setItem("accessToken", accessToken);
+      checkAuthentication(); // Actualiza el estado de autenticación
+    } catch (error) {
+      console.error("Error al iniciar sesión:", error.message);
+    }
+  };
+
   return (
     <BrowserRouter>
-    <div className="bg-white"> {/* Fondo blanco para toda la aplicación */}
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="bg-cover bg-center bg-no-repeat backgroundImage-back1">
-              <Login checkAuthentication={checkAuthentication} />
-            </div>
-          }
-        />-
-
-        {/* Protege todas las rutas dentro de "/home" */}
+        <Route path="/" element={<Login handleLogin={handleLogin} />} />
         <Route
           path="/home"
           element={
             isAuthenticated ? (
               <LayoutAdmin />
             ) : (
-              <Navigate to="/" replace={true} /> // Redirige al inicio de sesión si no está autenticado
+              <Navigate to="/" replace />
             )
           }
         >
           <Route index element={<Home />} />
           <Route path="verificacion" element={<Verificacion />} />
-        
         </Route>
-
         <Route path="*" element={<Error404 />} />
       </Routes>
-      </div>
     </BrowserRouter>
   );
 }
